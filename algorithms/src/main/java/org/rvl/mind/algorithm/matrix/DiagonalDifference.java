f74605848e5a4537d16f4ad4659c6caa87316697
romain vermorel <romain.vermorel@bell.ca>
2022-04-11 18:29:37 -0400

    algorithms

--- algorithms/src/main/java/org/rvl/mind/algorithm/matrix/DiagonalDifference.java
+++ algorithms/src/main/java/org/rvl/mind/algorithm/matrix/DiagonalDifference.java
@@ -1,2 +1,91 @@
-package org.rvl.mind.algorithm.matrix;public class DiagonalDifference {
+package org.rvl.mind.algorithm.matrix;
+
+import java.util.List;
+
+/**
+ * Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+ *
+ * For example, the square matrix  is shown below:
+ *
+ * 1 2 3
+ * 4 5 6
+ * 9 8 9
+ * The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+ *
+ * Function description
+ *
+ * Complete the  function in the editor below.
+ *
+ * diagonalDifference takes the following parameter:
+ *
+ * int arr[n][m]: an array of integers
+ * Return
+ *
+ * int: the absolute diagonal difference
+ * Input Format
+ *
+ * The first line contains a single integer, , the number of rows and columns in the square matrix .
+ * Each of the next  lines describes a row, , and consists of  space-separated integers .
+ *
+ * Constraints
+ *
+ * Output Format
+ *
+ * Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+ *
+ * Sample Input
+ *
+ * 3
+ * 11 2 4
+ * 4 5 6
+ * 10 8 -12
+ * Sample Output
+ *
+ * 15
+ * Explanation
+ *
+ * The primary diagonal is:
+ *
+ * 11
+ *    5
+ *      -12
+ * Sum across the primary diagonal: 11 + 5 - 12 = 4
+ *
+ * The secondary diagonal is:
+ *
+ *      4
+ *    5
+ * 10
+ * Sum across the secondary diagonal: 4 + 5 + 10 = 19
+ * Difference: |4 - 19| = 15
+ *
+ * Note: |x| is the absolute value of x
+ */
+public class DiagonalDifference {
+
+  public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+    int dimension = arr.get(0).size();
+
+    int lrX=0;
+    int rlX=0;
+    int lrY=0;
+    int rlY=dimension-1;
+    int sumLr=0;
+    int sumRl=0;
+
+    for(int row=0;row<dimension;row++){
+
+      sumLr += arr.get(lrX).get(lrY);
+      sumRl += arr.get(rlX).get(rlY);
+
+      lrX++;
+      lrY++;
+      rlX++;
+      rlY--;
+    }
+
+    return Math.abs(sumLr-sumRl);
+  }
+
 }
