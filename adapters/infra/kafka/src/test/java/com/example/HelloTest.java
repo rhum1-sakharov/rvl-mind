d16b8bcd8dc918f8810cebcfa50df611a844dfb0
romain vermorel <romain.vermorel@bell.ca>
2022-06-25 08:20:07 -0400

    move java-collections in algortihms

--- adapters/infra/kafka/src/test/java/com/example/HelloTest.java
+++ adapters/infra/kafka/src/test/java/com/example/HelloTest.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.KafkaContainer;
@@ -11,8 +11,8 @@ class HelloTest {
     private static final Logger logger = LoggerFactory.getLogger(HelloTest.class);
     private static final DockerImageName KAFKA_TEST_IMAGE = DockerImageName.parse("confluentinc/cp-kafka:6.2.1");
 
-    @Test
-    public void testUsage()  {
+    @Disabled
+    public void testUsage() {
         try (KafkaContainer kafka = new KafkaContainer(KAFKA_TEST_IMAGE)) {
             kafka.start();
             logger.info(kafka.getBootstrapServers());
---
+++ algorithms/src/main/java/org/rvl/mind/algorithm/sort/Swap.java
@@ -0,0 +1,14 @@
+package org.rvl.mind.algorithm.sort;
+
+import java.util.List;
+
+public class Swap {
+
+    public List<Integer> apply(List<Integer> list, int indexA, int indexB) {
+        Integer tmp = list.get(indexA);
+        list.set(indexA, list.get(indexB));
+        list.set(indexB, tmp);
+        return list;
+    }
+
+}
